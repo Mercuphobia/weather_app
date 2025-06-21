ae727b216a1830f9d218a2eeba926ad7f932b553
trungld <trungbia2002@gmail.com>
2025-06-21 09:57:35 +0700

    trungld: Delete redundant code

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import './cubit/weather_cubit.dart';
 import './repository/weather_repository.dart';
 import './view/weather_page.dart';
-
-
-// void main(){
-//   runApp(const WeatherApp());
-// }
-//
-// class WeatherApp extends StatelessWidget {
-//   const WeatherApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocProvider(
-//         create: (_) => WeatherCubit(WeatherRepository()),
-//         child: MaterialApp(
-//           home: WeatherPage(),
-//         ),
-//     );
-//   }
-// }
-
-
-import 'package:flutter/material.dart';
 
 
 
--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -1,28 +1,3 @@
-//
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import '../models/weather.dart';
-//
-// class WeatherRepository {
-//   final String apiKey = '9d28b3a7a7ae5bb89c43d46be45ad0cc';
-//
-//   Future<Weather> fetchWeather(String city) async{
-//     final url = 'https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey&lang=vi';
-//
-//     final response = await http.get(Uri.parse(url));
-//
-//     if(response.statusCode == 200){
-//       final json = jsonDecode(response.body);
-//       //print(json);
-//       return Weather.fromJson(json);
-//     }
-//     else{
-//       throw Exception('khong tim thay thanh pho');
-//     }
-//   }
-// }
-
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
